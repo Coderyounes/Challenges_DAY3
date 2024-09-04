4e395fa657ac5697a679a5dfe16981b6ca0f7241
Coderyounes <younesanas2021@gmail.com>
2024-09-04 23:50:46 +0100

    reverse string

---
+++ CH5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+    char phrase[10];
+    int count = 0, i, j;
+
+    printf("Enter the String: ");
+    scanf("%[^\n]", phrase);
+
+    for (i = 0; phrase[i] != '\0'; i++) {
+        count++;
+    }
+
+    for (j = count; j >= 0; j--) {
+        printf("%c", phrase[j]);
+    }
+    printf("\n");
+    return (0);
+}
