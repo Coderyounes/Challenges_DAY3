4f893a651f14e4def3077968f9176c9f3ad18dd9
Coderyounes <younesanas2021@gmail.com>
2024-09-05 00:02:08 +0100

    STruct contien un tableux

---
+++ CH_02_struct.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef struct perssone {
+    char nom[100];
+    char prenom[100];
+    int notes[5];
+} Personne_t;
+
+int main() {
+    int i, j;
+    Personne_t newPerson;
+
+    printf("Entree Nom: ");
+    scanf("%s", newPerson.nom);
+    getchar();
+    printf("Entree Prenom: ");
+    scanf("%s", newPerson.prenom);
+    for (i = 0; i < 5; i++) {
+        printf("Enter les notes: ");
+        scanf("%d", &newPerson.notes[i]);
+    }
+
+    printf("%s %s ", newPerson.nom, newPerson.prenom);
+
+    for (j = 0; j < 5; j++) {
+        printf("%d, ", newPerson.notes[j]);
+    }
+    printf("\n");
+    return (0);
+}
